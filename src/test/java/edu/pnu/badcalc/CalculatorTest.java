7fb1174363fe3988348845f86f16ed7377e694bd
Lamyaa Alotaibi <lamia635gg@gmail.com>
2025-11-15 06:09:28 +0300

    after test 2

--- src/test/java/edu/pnu/badcalc/CalculatorTest.java
+++ src/test/java/edu/pnu/badcalc/CalculatorTest.java
@@ -15,10 +15,10 @@ class CalculatorTest {
    @Test
    void testMultiply() {
        Calculator c = new Calculator();
-       assertEquals(6.0, c.multiply(2, 3));
+       assertEquals(6.09, c.multiply(2.9, 2.1), 0.0001);
    }
    @Test
-   void testDivide() {
+   void testDivideNormal() {
        Calculator c = new Calculator();
        assertEquals(2.0, c.divide(6, 3));
    }
@@ -28,9 +28,19 @@ class CalculatorTest {
        assertEquals(Double.POSITIVE_INFINITY, c.divide(10, 0));
    }
    @Test
-   void testFactorial() {
+   void testFactorialBasic() {
        Calculator c = new Calculator();
        assertEquals(120, c.factorial(5));
+   }
+   @Test
+   void testFactorialZero() {
+       Calculator c = new Calculator();
+       assertEquals(1, c.factorial(0));
+   }
+   @Test
+   void testFactorialNegative() {
+       Calculator c = new Calculator();
+       assertEquals(1, c.factorial(-4));
    }
    @Test
    void testParseAdd() {
@@ -45,11 +55,16 @@ class CalculatorTest {
    @Test
    void testParseMultiply() {
        Calculator c = new Calculator();
-       assertEquals(6.0, c.parseAndCompute("2 * 3"));
+       assertEquals(6.09, c.parseAndCompute("2.9 * 2.1"), 0.0001);
    }
    @Test
    void testParseDivide() {
        Calculator c = new Calculator();
        assertEquals(2.0, c.parseAndCompute("6 / 3"));
+   }
+   @Test
+   void testParseInvalidOp() {
+       Calculator c = new Calculator();
+       assertEquals(0.0, c.parseAndCompute("5 ^ 2"));
    }
 }
