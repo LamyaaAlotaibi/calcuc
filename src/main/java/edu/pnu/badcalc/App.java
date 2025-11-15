982072a253be706e102167c4cc1979faaa0301f0
Lamyaa Alotaibi <lamia635gg@gmail.com>
2025-11-15 05:23:14 +0300

    fix sonar issues

--- src/main/java/edu/pnu/badcalc/App.java
+++ src/main/java/edu/pnu/badcalc/App.java
@@ -1,18 +1,23 @@
 package edu.pnu.badcalc;
 import java.util.Scanner;
+import java.util.logging.Logger;
+import edu.pnu.badcalc.Calculator;
 public class App {
-   public static Calculator c = new Calculator();
-   static Scanner s = new Scanner(System.in);
+   private static final Logger logger = Logger.getLogger(App.class.getName());
+   private static final Calculator c = new Calculator();
+   private static final Scanner s = new Scanner(System.in);
    public static void main(String[] args) {
-       System.out.println("Bad Calculator v0.0.1");
+       logger.info("Bad Calculator v0.0.1");
        while (true) {
            System.out.print("> ");
            String line = s.nextLine();
-           if (line.equals("quit")) break;
+           if (line.equals("quit"))
+               break;
            try {
-               System.out.println("= " + c.parseAndCompute(line));
+               double result = c.parseAndCompute(line);
+               logger.info("= " + result);
            } catch (Exception e) {
-               System.out.println("err");
+               logger.warning("err");
            }
        }
    }
